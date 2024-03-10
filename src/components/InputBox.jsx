73c7994e337f5f8dda28a8e51f70b2207225c789
sagar <myemail@email.com>
2024-03-10 13:03:30 +0545

    first UI upgraded

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -13,17 +13,17 @@ function InputBox({
 }) {
   const labelId = useId();
   return (
-    <div className={`bg-white p-3 rounded-lg flex mb-2 ${className}`}>
+    <div className={`fromDivColor p-3 rounded-lg flex mb-2 ${className}`}>
       <div className="w-1/2">
         {/* LABEL */}
-        <label htmlFor={labelId} className="text-black/40 mb-2 inline-block">
+        <label htmlFor={labelId} className="labelColor mb-2 inline-block">
           {label}
         </label>
 
         {/* INPUT ELEMENT */}
         <input
           id={labelId}
-          className="outline-none w-full bg-transparent py-1.5"
+          className="inputColor outline-none w-full bg-transparent py-1.5"
           type="number"
           placeholder="Amount"
           disabled={amountDisable}
@@ -38,7 +38,7 @@ function InputBox({
       <div className="w-1/2 flex flex-wrap justify-end text-right">
         <p className="text-black/40 mb-2 w-full">Currency Type</p>
         <select
-          className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+          className="rounded-lg px-1 py-1 selectColor cursor-pointer outline-none"
           value={selectCurrency}
           onChange={(e) => {
             onCurrencyChange && onCurrencyChange(e.target.value);
